e0f28e3e01a6e7144abb9025228f09c50c1a3255
cakuai <angel09931935@gmail.com>
2020-10-05 23:42:51 +0800

    update the converter file

--- JPGtoPNG_converter.py
+++ JPGtoPNG_converter.py
@@ -6,16 +6,16 @@ from PIL import Image, ImageFilter
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-# check if new/ exists, if not create one
-
+# check if new/ folder exists, if not, then create one
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
-# loop through pokedex
 
+# loop through pokedex
 for filename in os.listdir(image_folder):
     img = Image.open(f"{image_folder}{filename}")
     # assume user will enter "/" for folder
-    cleanname = os.path.splitext(img)
-    # convert images to png
+    cleanname = os.path.splitext(filename)[0]
+    # convert images to png and also save them
     img.save(f"{output_folder}{cleanname}.png", "png")
-# save them to the new folder
+    print("The file is successfully converted!")
+
